958a5f840df87169ae8b633a3331c230cd1ff7ee
eddiedongatus@gmail.com <eddiedongatus@gmail.com>
2018-12-14 22:16:46 -0800

    inital setup

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path'); // node build in module for resolving ../ path
+// join method get rid of ../ path, avoid in and out of a dir
+const publicPath = path.join(__dirname, '../public'); // __dirname meaning current dir
+const express = require('express');
+const port = proces.env.PORT || 3000;
+const app = express();
+//server public directry
+app.use(express.static(publicPath));
+
+
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`);
+});
+
