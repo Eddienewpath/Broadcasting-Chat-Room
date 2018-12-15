35a9758f76b7554fdd4d66115cccad913340377c
eddiedongatus@gmail.com <eddiedongatus@gmail.com>
2018-12-14 22:32:31 -0800

    fix err

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path'); // node build in module for resolving ../ path
 // join method get rid of ../ path, avoid in and out of a dir
 const publicPath = path.join(__dirname, '../public'); // __dirname meaning current dir
 const express = require('express');
-const port = proces.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 //server public directry
 app.use(express.static(publicPath));
@@ -12,4 +12,3 @@ app.listen(port, () => {
     console.log(`server is up on port ${port}`);
 });
 
-    
